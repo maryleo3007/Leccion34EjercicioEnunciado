04b4c68f50ec4987410fea5cde0f86136e34a981
Misheca <Misheca>
2017-05-22 23:56:59 -0500

    add section ourpromise

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ var closealert = document.getElementById('closeAlert');
 closealert.addEventListener("click", function () {
   var alertTopClose = document.getElementById("alertTopClose");
   alertTopClose.classList.add("closeAlertTop");
+  
 });
